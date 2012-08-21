2432390bed5c5389a75c081ec01866a4f9afb204
solariums <solarium@qq.com>
2012-08-21 19:54:09 +0800

    change array.cpp

--- study_src/array.cpp
+++ study_src/array.cpp
@@ -7,7 +7,7 @@ int main(){
     int ia[array_size];
     int ar[] = {1,45,3,5,0};
     for(size_t ix=0; ix!= array_size; ++ix){
-        //        cout << "元素当前值是:" << ia[ix]<<endl;
+                cout << "元素当前值是:" << ia[ix]<<endl;
         if(ar[ix]){
             ia[ix]= ar[ix];
         }else{
